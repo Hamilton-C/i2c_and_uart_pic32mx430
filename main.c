9a18f293f053e5d2990e8703fbe461c70d066c25
Hamilton Canoquela <110467889+Hamilton-C@users.noreply.github.com>
2023-06-12 15:17:01 +0100

    main.c

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 /*
  * File:   main.c
- * Author: mcuser
+ * Author: Hamilton Canoquela
  *
  * Created on 9 de Junho de 2023, 16:36
  */
@@ -47,19 +47,19 @@ void main(void) {
     
         I2C2_stop();
 
-        while (U1STAbits.UTXBF);                                // Aguarda o buffer de transmiss„o estar vazio
+        while (U1STAbits.UTXBF);                                // Aguarda o buffer de transmiss√£o estar vazio
         U1TXREG = (Temp & 0xFF00) >> 8;                           // Envia o caractere
     
-        while (U1STAbits.UTXBF);                                // Aguarda o buffer de transmiss„o estar vazio
+        while (U1STAbits.UTXBF);                                // Aguarda o buffer de transmiss√£o estar vazio
         U1TXREG = Temp & 0x00FF;  
         
-        while (U1STAbits.UTXBF);                                // Aguarda o buffer de transmiss„o estar vazio
+        while (U1STAbits.UTXBF);                                // Aguarda o buffer de transmiss√£o estar vazio
         U1TXREG = (Hum & 0xFF00) >> 8;                           // Envia o caractere
     
-        while (U1STAbits.UTXBF);                                // Aguarda o buffer de transmiss„o estar vazio
+        while (U1STAbits.UTXBF);                                // Aguarda o buffer de transmiss√£o estar vazio
         U1TXREG = Hum & 0x00FF;  
         
-        while (U1STAbits.UTXBF);                                // Aguarda o buffer de transmiss„o estar vazio
+        while (U1STAbits.UTXBF);                                // Aguarda o buffer de transmiss√£o estar vazio
         U1TXREG = 0x0A;       
         delay_ms(2500);
     }
